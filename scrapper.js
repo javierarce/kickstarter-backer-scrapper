8a3f233be3bf109d29a8765ce5fc5cad63f43920
Javier Arce <javierarce@gmail.com>
2014-07-26 10:39:10 +0200

    first import

---
+++ scrapper.js
@@ -0,0 +1,75 @@
+var colorizer = require('colorizer').create('Colorizer');
+
+var casper = require('casper').create();
+
+var BACKERS_URL            = "https://www.kickstarter.com/projects/jennifercox/teeth-a-short-film/backers";
+var WAIT_BETWEEN_SCROLLING = 1000;
+
+function log(msg, color) {
+  console.log(colorizer.format(msg, { fg: color } ));
+}
+
+function scroll(casper) {
+
+  casper.waitFor(function() {
+
+    this.scrollToBottom();
+    this.wait(WAIT_BETWEEN_SCROLLING);
+
+    return true;
+
+  }, function() {
+
+    var info      = this.getElementInfo('div.load_more');
+    var backers   = this.getElementsInfo(".NS_backers__backing_row");
+    var locations = this.getElementsInfo(".NS_backers__backing_row .location");
+
+    log(backers.length + " backers loaded with "  + locations.length + " locations", "white");
+
+    if (info["visible"]) {
+
+      this.emit('load.more');
+
+    } else {
+
+      log("We're done. Open locations.txt to see the captured locations.", "green");
+
+    }
+
+  }, function() {
+
+    log("Unexpected error", "red");
+
+    exit();
+
+  }, 5000);
+
+}
+
+casper.start(BACKERS_URL, function() {
+
+  var info = this.getElementsInfo("[itemprop='Project[backers_count]']")
+
+  log("Getting location from " + info[0].text + " backers", "green");
+
+  scroll(this);
+
+});
+
+casper.on('load.more', function () {
+  scroll(this);
+});
+
+casper.then(function() {
+
+  var locations = [];
+
+  casper.each(this.getElementsInfo('.NS_backers__backing_row .location'), function(casper, element, j) {
+    locations.push(element["text"].trim());
+  });
+
+  require('fs').write("locations.txt", locations.join("\n"), 'w');
+
+});
+
+casper.run()
